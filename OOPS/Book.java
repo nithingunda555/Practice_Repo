ad815b4236534cea1fa6f42a80f2b1cde3f58237
nithingunda555 <160220L030@gmail.com>
2024-12-29 00:07:58 +0530

    Create Book.java

---
+++ OOPS/Book.java
@@ -0,0 +1,12 @@
+public class Book {
+	private int noOfCopies;
+
+	public void setNoOfCopies(int noOfCopies) {
+		this.noOfCopies = noOfCopies;
+	}
+
+	void BookPrinting() {
+		System.out.println("Book");
+	}
+
+}
