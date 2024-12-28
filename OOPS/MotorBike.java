331aed20ecbe5a7915e56f433b40c33795815acb
nithingunda555 <160220L030@gmail.com>
2024-12-29 00:09:28 +0530

    Create MotorBike.java

---
+++ OOPS/MotorBike.java
@@ -0,0 +1,17 @@
+public class MotoBike {
+	private int speed;
+
+	void start() {
+		System.out.println("Bike Started");
+
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+}
